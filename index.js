542b52dba9da57969dc26afbd9485dd70f250d5e
jottave <joao_victorflamengo10@outlook.com>
2024-08-07 11:16:19 -0300

    chore: add port in env

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 import 'dotenv/config.js'
 import express from 'express'
-import { PostgresHelper } from './src/db/postgres/helper.js'
 
 const app = express()
+app.use(express.json())
 
-app.get('/', async (req, res) => {
-    const results = await PostgresHelper.query('SELECT * FROM users;')
-    res.send(JSON.stringify(results))
-})
-
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`)
 })
