741b8b0b8ded5cacf9a922cf8f8643c55f052bda
jottave <joao_victorflamengo10@outlook.com>
2024-08-22 16:48:12 -0300

    feat: add returning in query sql

--- src/repositories/postgres/create-user.js
+++ src/repositories/postgres/create-user.js
@@ -1,9 +1,9 @@
-import { PostgresHelper } from '../../db/postgres/helper'
+import { PostgresHelper } from '../../db/postgres/helper.js'
 
 export class PostgresCreateUserRepository {
     async execute(userParams) {
         const results = await PostgresHelper.query(
-            `INSERT INTO users (id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)`,
+            `INSERT INTO users (id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
             [
                 userParams.id,
                 userParams.first_name,
