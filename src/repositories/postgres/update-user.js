eab8cdd3ca60afb5258a5a5d1fe625fc28f273a1
jottave <joao_victorflamengo10@outlook.com>
2024-08-23 16:20:28 -0300

    feat: add PostgresUpdateUserRepository

---
+++ src/repositories/postgres/update-user.js
@@ -0,0 +1,21 @@
+import { PostgresHelper } from '../../../helpers/postgres-helper.js'
+
+export class PostgresUpdateUserRepository {
+    async execute(userId, userParams) {
+        const updateFields = []
+        const updateValues = []
+        Object.keys(userParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateValues.length + 1}`)
+            updateValues.push(userParams[key])
+        })
+        updateValues.push = userId
+        const updateQuery = `
+        UPDATE users 
+        SET ${updateFields.join(', ')}
+        WHERE id = $${updateValues.length}
+        RETURNING *
+        `
+        const results = await PostgresHelper.query(updateQuery, updateValues)
+        return results[0]
+    }
+}
