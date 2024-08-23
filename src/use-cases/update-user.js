86b49ec98431b8db0b8cc87a46208c7d1274ed16
jottave <joao_victorflamengo10@outlook.com>
2024-08-23 16:42:14 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,29 @@
+import bcrypt from 'bcrypt'
+import { EmailAlreadyInUseError } from '../errors/user.js'
+
+export class UpdateUserUseCase {
+    constructor(postgresUpdateUserRepository, getUserByEmailRepository) {
+        this.postgresUpdateUserRepository = postgresUpdateUserRepository
+        this.getUserByEmailRepository = getUserByEmailRepository
+    }
+
+    async execute(userId, userParams) {
+        if (userParams.email) {
+            const userWithProvidedEmail =
+                await this.getUserByEmailRepository.execute(userParams.email)
+            if (userWithProvidedEmail && userWithProvidedEmail.id !== userId) {
+                throw new EmailAlreadyInUseError(userParams.email)
+            }
+        }
+        const user = { ...userParams }
+        if (userParams.password) {
+            const hashedPassword = await bcrypt.hash(userParams.password, 10)
+            user.password = hashedPassword
+        }
+        const updatedUser = await this.postgresUpdateUserRepository.execute(
+            userId,
+            user,
+        )
+        return updatedUser
+    }
+}
