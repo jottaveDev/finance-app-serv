65f1a9f0e3b611b19a1b1a463bb7e12e3e05e894
jottave <joao_victorflamengo10@outlook.com>
2024-08-26 16:29:04 -0300

    feat: add CreateTransactionUseCase and UserNotFoundError

---
+++ src/use-cases/transaction/create-transaction.js
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from 'uuid'
+import { UserNotFoundError } from '../../errors/user'
+
+export class CreateTransactionUseCase {
+    constructor(createTransactionRepository, getUserByIdRepository) {
+        this.createTransactionRepository = createTransactionRepository
+        this.getUserByIdRepository = getUserByIdRepository
+    }
+
+    async execute(transactionParams) {
+        const userId = transactionParams.userId
+        const user = await this.getUserByIdRepository.execute(userId)
+        if (!user) {
+            throw new UserNotFoundError(userId)
+        }
+        const transactionId = uuidv4()
+        const transaction = await this.createTransactionRepository.execute({
+            ...transactionParams,
+            id: transactionId,
+        })
+        return transaction
+    }
+}
