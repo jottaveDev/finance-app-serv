d91e45a4f33b7924b10b2989f0ae26d1f32ea258
jottave <joao_victorflamengo10@outlook.com>
2024-09-16 16:55:06 -0300

    feat: add DeleteTransactionController

---
+++ src/controllers/transaction/delete-transaction.js
@@ -0,0 +1,28 @@
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    notFound,
+    serverError,
+} from '../helpers/index.js'
+
+export class DeleteTransactionController {
+    constructor(deleteTransactionUseCase) {
+        this.deleteTransactionUseCase = deleteTransactionUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const { transactionId } = httpRequest.params
+            const idIsValid = checkIfIdIsValid(transactionId)
+            if (!idIsValid) return invalidIdResponse()
+            const deletedTransaction =
+                await this.deleteTransactionUseCase.execute(transactionId)
+            if (!deletedTransaction)
+                return notFound({ message: 'Transaction not found' })
+            return deletedTransaction
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}
